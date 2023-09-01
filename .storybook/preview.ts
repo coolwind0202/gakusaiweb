982f0c34b36d16ff6521b31aa4d0ae3d133fb81a
coolwind0202 <tororo_shidoro@yahoo.co.jp>
2023-09-02 00:12:20 +0900

    chore: MUIをインストールし、Storybookを設定

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,6 @@
-import type { Preview } from "@storybook/react";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { withThemeFromJSXProvider } from "@storybook/addon-themes";
+import type { Preview, ReactRenderer } from "@storybook/react";
 
 const preview: Preview = {
   parameters: {
@@ -10,6 +12,17 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    withThemeFromJSXProvider<ReactRenderer>({
+      themes: {
+        light: createTheme(),
+        //dark: createTheme({ palette: {mode: "dark"}}),
+      },
+      defaultTheme: "light",
+      Provider: ThemeProvider,
+      GlobalStyles: CssBaseline,
+    }),
+  ],
 };
 
 export default preview;
