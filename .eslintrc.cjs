24e0d4d670d6b335ebfbc535b1cacb28ac68667f
coolwind0202 <tororo_shidoro@yahoo.co.jp>
2023-09-03 01:01:55 +0900

    chore: themeとMUIのimport元を制限

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,41 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:storybook/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "import/no-restricted-paths": [
+      "error",
+      {
+        zones: [
+          {
+            from: "./src/features/theme/**/*",
+            target: "./src/features/!(theme)/**/*",
+          },
+          {
+            from: "node_modules/@mui/material",
+            target: "./src/features/!(ui|theme)/**/*",
+          },
+        ],
+      },
+    ],
   },
-}
+  settings: {
+    "import/resolver": {
+      typescript: {},
+    },
+  },
+};
